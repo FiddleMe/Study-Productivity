71c60cee5d7948cb873d66f4fa5b0b4b1e2cd440
FlyingGooseFace <65017180+FlyingGooseFace@users.noreply.github.com>
2022-10-17 15:22:45 +0800

    Update timer.js

--- js/timer/timer.js
+++ js/timer/timer.js
@@ -5,8 +5,7 @@ function display_countdown_timer(hours,minutes){
     var time = (hours * 60 * 60 + minutes * 60) * 1000
     // Update the count down every 1 second
     var x = setInterval(function() {
-
-        // Find the time between now and the count down date
+        
         time = time - 1000;
 
         // Time calculations for hours, minutes and seconds
@@ -25,3 +24,37 @@ function display_countdown_timer(hours,minutes){
         }
     }, 1000);
 }
+
+//get time in miliseconds 
+//do on session start and end:
+function get_time(){
+    var d = new Date();
+    var time = d.getTime();
+    console.log(d,time);
+    return time
+}
+
+//add time in minutes to database after session ends
+async function add_time(start, end, userId){
+    var time = (end - start) / 60000 ;
+        console.log(time);
+    var ref = doc(db, "Users", userId);
+
+    const docSnap = await getDoc(ref);
+        
+    var curr_time = docSnap.data().TotalTime;
+        console.log(curr_time);
+    time += curr_time;
+        console.log(time);
+    var data = {
+        TotalTime: time
+    }
+    updateDoc(ref, data)
+    .then(ref => {
+        console.log("time updated");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
