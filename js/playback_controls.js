27fccf9d5402e1593c07f17f27934467f64ee867
FlyingGooseFace <65017180+FlyingGooseFace@users.noreply.github.com>
2022-10-26 14:08:46 +0800

    Add files via upload

---
+++ js/playback_controls.js
@@ -0,0 +1,196 @@
+//To be placed on session page
+//initialises spotify web player
+//Add controls to:
+    //start/stop => id="togglePlay"
+    //next => id="next"
+    //back => id="back"
+    // document.getElementsByTagName("BODY")[0].addEventListener("load", startSpotify, false);
+//document.body.addEventListener("load", startSpotify);
+
+
+if (window.location.hash.replace("#access_token=","").split("&")[0]){
+    console.log("yes");
+    startSpotify();
+}
+function startSpotify() {
+    var spotify_script = document.createElement('script');
+
+    spotify_script.setAttribute('src','https://sdk.scdn.co/spotify-player.js');
+
+    document.head.appendChild(spotify_script);
+
+    var token = window.location.hash.replace("#access_token=","").split("&")[0];
+    // token = token.replace("#access_token=","");
+    // token = token.split("&")[0];
+    console.log(token);
+    
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new Spotify.Player({
+            name: "WebApp Music Player",
+            getOAuthToken: (cb) => {
+            cb(token);
+            },
+            volume: 0.5,
+        });
+    
+        // Player Ready
+        player.addListener("ready", ({ device_id }) => {
+            console.log("Ready with Device ID", device_id);
+    
+            // After player is ready, change current device to this player
+            const connect_to_device = () => {
+            console.log("Changing to device");
+            let change_device = fetch("https://api.spotify.com/v1/me/player", {
+                method: "PUT",
+                body: JSON.stringify({
+                device_ids: [device_id],
+                play: false,
+                }),
+                headers: new Headers({
+                Authorization: "Bearer " + token,
+                }),
+            }).then((response) => console.log(response));
+            };
+            connect_to_device();
+        });
+    
+        // Not Ready
+        player.addListener("not_ready", ({ device_id }) => {
+            console.log("Device ID has gone offline", device_id);
+        });
+    
+        // Error Handling
+        player.addListener("initialization_error", ({ message }) => {
+            console.error(message);
+        });
+        player.addListener("authentication_error", ({ message }) => {
+            console.error(message);
+        });
+        player.addListener("account_error", ({ message }) => {
+            console.error(message);
+        });
+    
+        // Start device connection
+        player.connect().then((success) => {
+            if (success) {
+            console.log("The Web Playback SDK successfully connected to Spotify!");
+            }
+        });
+    
+        // Toggle Play Button
+        document.getElementById("togglePlay").onclick = function () {
+            player.togglePlay();
+        };
+    
+        document.getElementById('next').onclick = function next() {
+                player.nextTrack();
+        };
+        
+        document.getElementById('back').onclick = function() {
+            player.previousTrack();
+        };
+    
+    };
+    async function get_playlist(){
+        console.log("Getting Playlist");
+        const response = await fetch("https://api.spotify.com/v1/me/playlists",
+        {
+            method: "GET",
+            headers: new Headers({
+            Authorization: "Bearer " + token,
+            }),
+        }
+        )
+        var data = await response.json();
+        console.log(data);
+        var uris = new Array;
+        for(i in data.items){
+            let name = data.items[i].name
+            let uri = data.items[i].uri
+            console.log(name)
+            const template = document.createElement('li');
+            template.innerHTML = name;
+            document.getElementById("playlists").appendChild(template);
+            //Display playlists
+            uri = uri.split(":")[2]
+            console.log(uri)
+            uris.push(uri)
+        }
+        console.log(uris);
+        return uris
+    }
+    
+    
+    //2. Get song uris from playlist uri
+    //input:str playlist_uri
+    //output: array[str song_uri]
+    async function get_songs_from_playlist(playlist_uri){
+        console.log("Getting Song From Playlist");
+        const response = await 
+        fetch("https://api.spotify.com/v1/playlists/"+playlist_uri+"/tracks",
+            {
+                method: "GET",
+                headers: new Headers({
+                Authorization: "Bearer " + token,
+                }),
+            })
+        var data = await response.json();
+        console.log(data);
+        console.log(data.items[0].track.uri);
+    
+        var songs = new Array;
+        for (i in data.items){
+            songs.push(data.items[i].track.uri)
+        }
+        console.log(songs);
+        return songs
+    }
+    
+    
+    //3.Play Song(s)
+    //input:array[str uri]
+    //output:
+    function play_song(uri){
+        console.log("Changing song");
+        fetch(
+            "https://api.spotify.com/v1/me/player/play",
+            {
+            method: "PUT",
+            body: JSON.stringify({
+                uris: uri,
+            }),
+            headers: new Headers({
+                Authorization: "Bearer " + token,
+            }),
+            }
+        )
+        .then((data) => console.log(data));
+        };
+    
+    async function test (){
+        var playlist_uris = await get_playlist();
+            console.log(playlist_uris);
+    
+        var one_playlist_uris = playlist_uris[1];
+            console.log(one_playlist_uris);
+    
+        var song_uris = await get_songs_from_playlist(one_playlist_uris);
+            console.log(song_uris);
+        
+        document.getElementById('playlisttest').onclick = function() {
+            play_song(song_uris);
+        }
+        
+    }  
+        
+    document.getElementById('playlisttest').onclick = function() {
+        test();
+    }
+    
+}
+
+//to be placed on session page
+
+//1. Get playlist uris from user
+//input:
+//output: array[str playlist_uri]
---
+++ js/spotify_auth.js
@@ -0,0 +1,17 @@
+//to be placed on wherever start session is
+//authenticate on click of id="auth"
+
+//to be changed to session page's url
+//personal note: remember to add to spotify's app dashboard
+const redirect_uri = "http://127.0.0.1:55313/mainpage.html";
+//
+
+const client_id = "8829529abdc04af28a7fbc4428d376b6";
+const scopes = "user-library-read user-read-email user-read-private user-read-playback-state playlist-read-private user-follow-read user-read-currently-playing user-read-playback-position streaming user-read-recently-played user-top-read user-modify-playback-state playlist-read-collaborative";
+var url = "https://accounts.spotify.com/authorize";
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scopes);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+console.log(url);
+document.getElementById("auth").addEventListener("click", function(){location.assign(url)});
