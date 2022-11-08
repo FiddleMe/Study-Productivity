3b8666db0f49eb7049282aabdeca845dfda08342
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2022-11-08 19:04:47 +0800

    update timer functionality

--- js/timer.js
+++ js/timer.js
@@ -1,21 +1,30 @@
 // input time(hrs, mins) specified by user
 //updates element with id="timer" with a countdown timer
 
+var x;
 
 function displayTime(){
     display_countdown_timer(document.getElementById('hours').value, document.getElementById('minutes').value)
 }
 
-// function endTime(){
-//     clearInterval(x);
-//     isDone = true
-//     document.getElementById("timer").innerHTML = "Start Again?";
-//     document.getElementById("progressBar").innerHTML = ''
-//     document.getElementById('input1').style.display = 'block'
-//     document.getElementById('input2').style.display = 'block'
-// }
+function endTime(){
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Start Again?";
+    document.getElementById('save').style.display = ''
+    document.getElementById('end').style.display = 'none'
+    document.getElementById('bar').style.display = 'none'
+    // document.getElementById("progressBar").innerHTML = 'none'
+    document.getElementById('input1').style.display = ''
+    document.getElementById('input2').style.display = ''
+    // isDone = true
+    // document.getElementById("timer").innerHTML = "Start Again?";
+    // document.getElementById("progressBar").innerHTML = ''
+    // document.getElementById('input1').style.display = ''
+    // document.getElementById('input2').style.display = ''
+}
 
 function display_countdown_timer(hours,minutes){
+    // endTime()
     var time = (hours * 60 * 60 + minutes * 60) * 1000
     var initialTime = time
     // Update the count down every 1 second
@@ -37,6 +46,10 @@ function display_countdown_timer(hours,minutes){
 
     document.getElementById('input1').style.display = 'none'
     document.getElementById('input2').style.display = 'none'
+    document.getElementById('save').style.display = 'none'
+
+    document.getElementById('end').style.display = ''
+    document.getElementById('bar').style.display = ''
     // console.log( document.getElementById('input1').style.display)
     // document.getElementById('input2').innerHTML = ''
     // buttonAct.removeEventListener('click', displayTime)
@@ -45,7 +58,7 @@ function display_countdown_timer(hours,minutes){
 
     
 
-    var x = setInterval(function() {
+    x = setInterval(function() {
         
         let progresswidth =  time / initialTime * 100
         // console.log(progresswidth)
@@ -70,26 +83,27 @@ function display_countdown_timer(hours,minutes){
 
         // If the count down is finished, update the element with id="timer"
         if (time < 0) {
-            clearInterval(x);
+            // clearInterval(x);
             isDone = true
-            document.getElementById("timer").innerHTML = "Start Again?";
-            document.getElementById("progressBar").innerHTML = ''
-            document.getElementById('input1').style.display = ''
-            document.getElementById('input2').style.display = ''
+            // document.getElementById("timer").innerHTML = "Start Again?";
+            // document.getElementById('save').style.display = ''
+            // document.getElementById('end').style.display = 'none'
+            // document.getElementById('bar').style.display = 'none'
+            // // document.getElementById("progressBar").innerHTML = 'none'
+            // document.getElementById('input1').style.display = ''
+            // document.getElementById('input2').style.display = ''
+            endTime()
 
             // document.getElementById('input1').innerHTML = `<span class="input-group-text" id="basic-addon1">Hours</span>
             // <input type="text" class="form-control" id="hours" name="hours" value="" aria-label="Username" aria-describedby="basic-addon1">`
             // document.getElementById('input2').innerHTML = `<span class="input-group-text" id="basic-addon1">Minutes</span>
             // <input type="text" class="form-control" id="minutes" name="minutes" value="" aria-label="Username" aria-describedby="basic-addon1">`
         }
-
-        else{
-
-        }
         
-        if (time == original_time/2){
+        else if (time == original_time/2){
             isHalf = true
         }
+
         if(!isHalf && !isDone){
             document.getElementById('gif_now').innerHTML = '<img src="gifs/gif2.gif" id="gif_now" style="margin-top: 7px; height:50px; width: 150px">'
         }
