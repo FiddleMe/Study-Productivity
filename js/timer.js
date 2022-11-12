a682d5a90cdbfd0291a7cf9d4366d966d9b15661
Deric <iamsomoene@gmail.com>
2022-11-12 16:58:06 +0800

    fixed date

--- js/timer.js
+++ js/timer.js
@@ -221,7 +221,7 @@ function display_countdown_timer(hours,minutes){
             
             //add scores to databse using original time
             function formatDate(date){
-              var formatted_date = date.getDate().toString()+"/"+date.getMonth().toString()+"/"+date.getFullYear().toString();
+              var formatted_date = date.getDate().toString()+"/"+(date.getMonth()+1).toString()+"/"+date.getFullYear().toString();
               return formatted_date;
             }
             async function add_score(score, userId){
