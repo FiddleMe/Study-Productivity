a15067fa0b442051cbb382a6b5c7feb442512b3a
Deric <iamsomoene@gmail.com>
2022-11-13 23:15:22 +0800

    aaa

--- js/Spotify api/spotify_auth.js
+++ js/Spotify api/spotify_auth.js
@@ -3,7 +3,7 @@
 
 //to be changed to session page's url
 //personal note: remember to add to spotify's app dashboard
-const redirect_uri = "http://127.0.0.1:5501/index.html";
+const redirect_uri = "http://localhost/index.html";
 
 const client_id = "631594e2aa7342aa8301c901e1b822b0";
 const scopes = "user-library-read user-read-email user-read-private user-read-playback-state playlist-read-private user-follow-read user-read-currently-playing user-read-playback-position streaming user-read-recently-played user-top-read user-modify-playback-state playlist-read-collaborative";
